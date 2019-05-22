f039512cc5e9ceacc66cf09ae91b00f31a390c45
Vexten <niksh.hots.mail@gmail.com>
2019-05-22 17:55:16 +0300

    fixed things

--- Не Django/Лабораторная работа 4/Lab4_client.py
+++ Не Django/Лабораторная работа 4/Lab4_client.py
@@ -3,19 +3,27 @@ import socket
 io = socket.socket()
 op = ""
 while True:
-    try:
-        host = input("Введите IP адрес сервера: ")
-        io.connect((host,322))
-        break
-    except OSError:
-        print("Нет сервера по данному адресу.")
-while True:
-    while op == "":
-        op = input("Введите команду: ")
-    op = op.upper()
-    io.send(op.encode())
+    while True:
+        try:
+            host = input("Введите IP адрес сервера (0 - выход): ")
+            if host != "0": io.connect((host,5025))
+            else: op = "0"
+            break
+        except OSError:
+            print("Нет сервера по данному адресу.")
+    while True:
+        try:
+            while op == "":
+                op = input("Введите команду (help - помощь): ")
+            op = op.upper()
+            if op != "0": io.send(op.encode())
+            else: op = "EXIT"
+            if op == "EXIT": break
+            op = ""
+            inp = io.recv(1024).decode()
+            print(inp)
+        except ConnectionAbortedError:
+            print("Соединение с сервером разорвано.")
+            break
     if op == "EXIT": break
-    op = ""
-    inp = io.recv(1024).decode()
-    print(inp)
 io.close()
--- Не Django/Лабораторная работа 4/Lab4_server.py
+++ Не Django/Лабораторная работа 4/Lab4_server.py
@@ -3,7 +3,7 @@ import socket, datetime, locale
 locale.setlocale(locale.LC_ALL, "")
 
 io = socket.socket()
-io.bind(("",322))
+io.bind(("",5025))
 io.listen(1)
 
 while True:
@@ -14,23 +14,26 @@ while True:
             inp = cl.recv(1024).decode()
             try:
                 inp = inp.upper()
-                if inp == "DATE TIME":
+                if inp == "HELP":
+                    op = """date time - возвращает дату и время сервера
+es - выключение сервера
+exit - отключиться от сервера и закрыть клиент"""
+                elif inp == "DATE TIME":
                     op = datetime.datetime.now().strftime("%c")
                 elif inp == "EXIT":
                     print(addr,"отключился")
                     cl.close()
-                elif inp == "0":
+                elif inp == "ES":
                     op = "Выключение сервера..."
                     print(op)
                 else:
                     raise ValueError
                 if inp != "EXIT": cl.send(op.encode())
-                if inp == "0":
-                    break
+                else: break
+                if inp == "ES": break
             except ValueError:
                 cl.send("Такой команды нет.".encode())
-        if inp == "0":
-            break
+        if inp == "ES": break
     except ConnectionResetError:
         print(addr,"разорвал соединение.")
 cl.close()
